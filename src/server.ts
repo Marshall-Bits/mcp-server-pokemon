75b58ad83e65031b32c5e5ba4702d18894bc4189
Marshall-Bits <kz_@hotmail.es>
2025-12-04 17:42:15 +0100

    add pokemon tool

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import z from "zod";
 
 const server = new McpServer(
   {
@@ -14,6 +15,46 @@ const server = new McpServer(
     },
   }
 );
+
+server.registerTool(
+  "Consultar un pokemon",
+  {
+    title: "Consultar un pokemon",
+    inputSchema: {
+      name: z.string().describe("Nombre del pokemon a consultar"),
+    },
+    outputSchema: z.object({
+      name: z.string(),
+      height: z.number(),
+      weight: z.number(),
+    }).describe("Información del pokemon"),
+  },
+  async (params) => {
+    const { name } = params;
+    const pokemonData = await fetchPokemon(name);
+    return {content: [
+      {type: "text", text: `Nombre: ${pokemonData.name}`},
+      {type: "text", text: `Altura: ${pokemonData.height}`},
+      {type: "text", text: `Peso: ${pokemonData.weight}`},
+    ]};        
+  }
+);
+
+async function fetchPokemon(name: string) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
+  );
+  if (!response.ok) {
+    throw new Error(`Pokemon ${name} not found`);
+  }
+  const data = await response.json();
+  const cleanedData = {
+    name: data.name,
+    height: data.height,
+    weight: data.weight,
+  };
+  return cleanedData;
+}
 
 async function main() {
   const transport = new StdioServerTransport();
